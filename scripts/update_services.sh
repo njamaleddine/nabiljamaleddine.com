331d486c47a824b6ae326868b9803ca69426c284
Nabil Jamaleddine <njamaleddine@users.noreply.github.com>
2021-07-02 19:12:44 -0400

    fix typo

--- scripts/update_services.sh
+++ scripts/update_services.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo cp ./bin/ubuntu/services/nabiljamaleddine_web.service /etc/systemd/system/nabiljamaleddine_web.service
+sudo cp ./scripts/ubuntu/services/nabiljamaleddine_web.service /etc/systemd/system/nabiljamaleddine_web.service
 sudo systemctl daemon-reload
